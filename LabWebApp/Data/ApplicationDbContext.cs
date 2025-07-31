5962691f6521743788107575bc73bfa97562f813
mshib <mshib@SHIBILY>
2025-07-30 20:34:31 -0400

    Add project files.

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LabWebApp.Models;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
